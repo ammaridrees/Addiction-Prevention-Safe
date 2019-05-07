cae3065ffc63ddca6e104af1f3feeecb0d1cf765
Ammar Idrees <31746875+aidree3@users.noreply.github.com>
2019-05-07 17:41:04 -0500

    Update Password.ino
    New code includes some documentation and the ability to display the timer

--- Addiction Prevention Safe/Password/Password.ino
+++ Addiction Prevention Safe/Password/Password.ino
@@ -1,3 +1,14 @@
+//Group #60
+//Name: Maaz Ahmed, netID: mahmed76
+//Name: Hasan Ali, netID: hali32
+//Name: Ammar Idrees, netID: aidree3
+//Name: Umer Qazi, netID: uqazi2
+//Project name: Addiction Prevention Safe
+//Abstract: Our project is to implement a safe-like device that is designed to keep the user away from wasting their time on certain tasks (e.g. phone usage, etc).
+//The safe will use some security features such as the requirement of a password as well as a timer which will make the safe unable to open until the timer is complete. 
+//For example, if a user wants to stay away from their phone for a specific time, they can place their phone into the container,
+//and they will not be able to use their phone until it allows you to re-enter a password.
+
 /*The code here utilizes four buttons to enter a 4 digit pin that will be displayed on an LCD
  * screen. To set a password, it must be hardcoded into this program (currently 1 2 3 4). Note
  * that the wiring is a bit fragile and can easily make the LCD unreadable, if that is the case,
@@ -11,12 +22,13 @@ int buttons[] = {8, 9, 10, 11};
 //They will be all 0s by default
 int code[] = {0, 0, 0, 0};
 //Correct code is here
-int correctCode[] = {1, 2, 3, 4};
+int correctCode[] = {1, 4, 1, 8};
 //Set up LCD and create flag that will determince if correct code is entered
 LiquidCrystal lcd(13, 12, 5, 4, 3, 2);
 bool correct = false;
 bool isLocked = false;
 int timer = 30;
+
 
 void setup() {
   //All buttons will be input
@@ -97,7 +109,10 @@ void loop() {
     while (timer > 0) {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Please wait.. ");
+      lcd.print("Time remaining: ");
+      lcd.setCursor(3, 1);
+      lcd.print("00:00:");
+      lcd.print(timer);
       timer--;
       delay(1000);
     }
